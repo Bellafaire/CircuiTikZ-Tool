7263a6acc9ec6e63707ebb48fb71178120740826
Matthew Bellafaire <matthew.bellafaire@gmail.com>
2020-03-26 14:44:11 -0400

    implemented basic wire drawing

--- CircuitikzTool/src/circuitikztool/CircuitMaker.java
+++ CircuitikzTool/src/circuitikztool/CircuitMaker.java
@@ -8,6 +8,7 @@ package circuitikztool;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.MouseInfo;
+import java.awt.Point;
 import java.awt.Polygon;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -26,7 +27,8 @@ public class CircuitMaker extends JPanel {
 
     static int GRID_SIZE = 25;
 
-    int x_offset, y_offset = 0;
+    int x_offset = GRID_SIZE * 5;
+    int y_offset = GRID_SIZE * 5;
     boolean dragging = false;
     boolean clicking = false;
     int lastMouseX, lastMouseY;
@@ -34,11 +36,14 @@ public class CircuitMaker extends JPanel {
     int xGridPosition;
     int yGridPosition;
 
+    Point originOffset;
+
     ArrayList<Component> components;
 
     private static String[] availableTools = {"Wire", "Resistor", "Capacaitor"};
 
     static int currentTool = Component.WIRE;
+    private Point wireStart;
 
     public static void setCurrentTool(int tool) {
         currentTool = tool;
@@ -51,15 +56,15 @@ public class CircuitMaker extends JPanel {
 
             @Override
             public void mouseMoved(MouseEvent e) {
-                xGridPosition = (e.getX() / GRID_SIZE) * GRID_SIZE;
-                yGridPosition = (e.getY() / GRID_SIZE) * GRID_SIZE;
+                xGridPosition = (e.getX() / GRID_SIZE);
+                yGridPosition = (e.getY() / GRID_SIZE);
 
             }
 
             @Override
             public void mouseDragged(MouseEvent e) {
-                xGridPosition = (e.getX() / GRID_SIZE) * GRID_SIZE;
-                yGridPosition = (e.getY() / GRID_SIZE) * GRID_SIZE;
+                xGridPosition = (e.getX() / GRID_SIZE);
+                yGridPosition = (e.getY() / GRID_SIZE);
             }
         }
         );
@@ -73,11 +78,15 @@ public class CircuitMaker extends JPanel {
                     case MouseEvent.BUTTON1:
                         //left click
                         clicking = true;
+
+                        //wire's are a special case, since they require two points to be placed
+                        if (currentTool == Component.WIRE) {
+                            wireStart = new Point(xGridPosition - originOffset.x, yGridPosition - originOffset.y);
+                        }
                         break;
                     case MouseEvent.BUTTON2:
                         //center click
                         dragging = true;
-                        clicking = false;
                         lastMouseX = MouseInfo.getPointerInfo().getLocation().x;
                         lastMouseY = MouseInfo.getPointerInfo().getLocation().y;
                         break;
@@ -90,6 +99,10 @@ public class CircuitMaker extends JPanel {
             public void mouseReleased(MouseEvent e) {
                 if (e.getButton() == MouseEvent.BUTTON1) {
                     //left click
+                    clicking = false;
+                    if (currentTool == Component.WIRE) {
+                        placeWire();
+                    }
                 } else if (e.getButton() == MouseEvent.BUTTON2) {
                     //center click
                     dragging = false;
@@ -114,34 +127,42 @@ public class CircuitMaker extends JPanel {
         int originOffsetX = (x_offset / GRID_SIZE) * GRID_SIZE;
         int originOffsetY = (y_offset / GRID_SIZE) * GRID_SIZE;
 
+        originOffset = new Point(x_offset / GRID_SIZE, y_offset / GRID_SIZE);
+
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, 10000, 10000);
 
         //if someone hovers over the origin lets make sure they know that its the origin
-        if (originOffsetX == xGridPosition && originOffsetY == yGridPosition) {
+        if (originOffsetX == xGridPosition * GRID_SIZE && originOffsetY == yGridPosition * GRID_SIZE) {
             g.setColor(Color.white);
-            g.drawString("Origin", originOffsetX - 10, originOffsetY - 5 );
+            g.drawString("Origin", originOffsetX - 10, originOffsetY - 5);
         }
 
         //draw the grid
         g.setColor(Color.GRAY);
-        for (int x = 5; x < this.getWidth(); x += GRID_SIZE) {
-            for (int y = 5; y < this.getHeight(); y += GRID_SIZE) {
+        for (int x = 0; x < this.getWidth(); x += GRID_SIZE) {
+            for (int y = 0; y < this.getHeight(); y += GRID_SIZE) {
                 g.drawLine(x, y, x, y);
             }
         }
 
         //draw origin
         g.setColor(Color.BLUE);
-        g.fillOval(originOffsetX + 3, originOffsetY + 3, 5, 5);
+        g.fillOval(originOffsetX - 3, originOffsetY - 3, 5, 5);
 
         //draw the current mouse position snapped to the grid
         g.setColor(Color.white);
-        g.fillOval(xGridPosition + 2, yGridPosition + 2, 5, 5);
+        g.fillOval(GRID_SIZE * xGridPosition - 3, GRID_SIZE * yGridPosition - 3, 5, 5);
 
         if (clicking) {
             switch (currentTool) {
                 case Component.WIRE:
+                    g.setColor(Color.white);
+                    g.drawLine(
+                            GRID_SIZE * (wireStart.x + originOffset.x),
+                            GRID_SIZE * (wireStart.y + originOffset.y),
+                            GRID_SIZE * xGridPosition,
+                            GRID_SIZE * yGridPosition);
                     break;
                 case Component.TWO_TERMINAL:
                     break;
@@ -150,6 +171,10 @@ public class CircuitMaker extends JPanel {
                 default:
                     throw new IllegalStateException("Current Tool Selected does not exist");
             }
+        }
+
+        for (int a = 0; a < components.size(); a++) {
+            components.get(a).paint(g, GRID_SIZE, originOffset);
         }
 
     }
@@ -163,5 +188,9 @@ public class CircuitMaker extends JPanel {
             System.out.println("Failed to load image: " + filename);
         }
         return new BufferedImage(1, 1, 1);
+    }
+
+    public void placeWire() {
+        components.add(new Component(wireStart, new Point(xGridPosition - originOffset.x, yGridPosition - originOffset.y), Component.WIRE));
     }
 }
--- CircuitikzTool/src/circuitikztool/Component.java
+++ CircuitikzTool/src/circuitikztool/Component.java
@@ -6,29 +6,56 @@ import java.awt.image.BufferedImage;
 
 public class Component {
 
-    int x, y;
-    Point[] connectionPoints;
+    Point wireStart, wireEnd;
+    Point position;
     BufferedImage Icon;
     String name;
+    int componentType;
 
     final static int WIRE = 0;
     final static int TWO_TERMINAL = 1;
     final static int THREE_TERMINAL = 2;
 
     public Component(int grid_x, int grid_y, int componentSelected) {
-                if(componentSelected == WIRE){
+        if (componentSelected == WIRE) {
             throw new IllegalArgumentException("Component Selected == Wire but no start/end point is defined");
         }
+        componentType = componentSelected;
     }
 
     public Component(int grid_x_start, int grid_y_start, int grid_x_end, int grid_y_end, int componentSelected) {
-        if(componentSelected != WIRE){
+        if (componentSelected != WIRE) {
             throw new IllegalArgumentException("Component Selected != Wire");
         }
+        componentType = componentSelected;
     }
-    
-       public void paint(Graphics g, int gridSize) {
-       
-       }
+
+    public Component(Point wireStart, Point wireEnd, int componentSelected) {
+        if (componentSelected != WIRE) {
+            throw new IllegalArgumentException("Component Selected != Wire");
+        }
+        this.wireStart = wireStart;
+        this.wireEnd = wireEnd;
+        componentType = componentSelected;
+    }
+
+    public void paint(Graphics g, int gridSize, Point offset) {
+        switch (componentType) {
+            case WIRE:
+                g.drawLine(
+                        gridSize * (wireStart.x + offset.x),
+                        gridSize * (wireStart.y + offset.y),
+                        gridSize * (wireEnd.x + offset.x),
+                        gridSize * (wireEnd.y + offset.y)
+                );
+                break;
+            case TWO_TERMINAL:
+                break;
+            case THREE_TERMINAL:
+                break;
+            default:
+                break;
+        }
+    }
 
 }
